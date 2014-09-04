6f21a8fabd33b2cc3fc624eff211d826477ca1a3
Jim Higson <jim.higson@gmail.com>
2014-09-04 12:53:00 +0100

    package up a working event bus

---
+++ src/jus.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {   
+   functional: require('./functional.js'),
+   lists: require('./lists.js'),
+   pubSub: require('./pubSub.js'),
+   singleEventPubSub: require('./singleEventPubSub.js')
+};
